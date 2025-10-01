72dd32fa87841d3fc42bbc98de7930605599fca4
Thoon She Yi <thoonlay779@gmail.com>
2025-10-01 11:36:08 +0630

    Prev Change History

--- app/Http/Controllers/Api/ExchangeRatesController.php
+++ app/Http/Controllers/Api/ExchangeRatesController.php
@@ -124,37 +124,38 @@ class ExchangeRatesController extends Controller
                                         $changehistories = $exchangerate->changehistory()->orderBy('record_at','desc')->with('user')->get();
 
 
-                                        if($changehistories->count() <= 0){
-                                            $diffs = [
-                                                   // diffs
-                                                'diff_tt_buy'  => $yesterdayrate ? $exchangerate->tt_buy - $yesterdayrate->tt_buy : null,
-                                                'diff_tt_sell' => $yesterdayrate ? $exchangerate->tt_sell - $yesterdayrate->tt_sell : null,
-                                                'diff_cash_buy'  => $yesterdayrate ? $exchangerate->cash_buy - $yesterdayrate->cash_buy : null,
-                                                'diff_cash_sell' => $yesterdayrate ? $exchangerate->cash_sell - $yesterdayrate->cash_sell : null,
-                                                'diff_earn_buy'  => $yesterdayrate ? $exchangerate->earn_buy - $yesterdayrate->earn_buy : null,
-                                                'diff_earn_sell' => $yesterdayrate ? $exchangerate->earn_sell - $yesterdayrate->earn_sell : null,
-                                            ];
-                                        }else{
-                                            // $count = $changehistories->count();
-                                            // $lastchangehistory = $changehistories[$count -1];
-                                            // $diffs = [
-                                            //        // diffs
-                                            //     'diff_tt_buy'  => $lastchangehistory ? $lastchangehistory->tt_buy - $yesterdayrate->tt_buy : null,
-                                            //     'diff_tt_sell' => $lastchangehistory ? $lastchangehistory->tt_sell - $yesterdayrate->tt_sell : null,
-                                            //     'diff_cash_buy'  => $lastchangehistory ? $exchangerate->cash_buy - $yesterdayrate->cash_buy : null,
-                                            //     'diff_cash_sell' => $lastchangehistory ? $exchangerate->cash_sell - $yesterdayrate->cash_sell : null,
-                                            //     'diff_earn_buy'  => $lastchangehistory ? $exchangerate->earn_buy - $yesterdayrate->earn_buy : null,
-                                            //     'diff_earn_sell' => $lastchangehistory ? $exchangerate->earn_sell - $yesterdayrate->earn_sell : null,
-                                            // ];
-                                            $diffs = [
-                                                   // diffs
-                                                'diff_tt_buy'  => $yesterdayrate ? $exchangerate->tt_buy - $yesterdayrate->tt_buy : null,
-                                                'diff_tt_sell' => $yesterdayrate ? $exchangerate->tt_sell - $yesterdayrate->tt_sell : null,
-                                                'diff_cash_buy'  => $yesterdayrate ? $exchangerate->cash_buy - $yesterdayrate->cash_buy : null,
-                                                'diff_cash_sell' => $yesterdayrate ? $exchangerate->cash_sell - $yesterdayrate->cash_sell : null,
-                                                'diff_earn_buy'  => $yesterdayrate ? $exchangerate->earn_buy - $yesterdayrate->earn_buy : null,
-                                                'diff_earn_sell' => $yesterdayrate ? $exchangerate->earn_sell - $yesterdayrate->earn_sell : null,
-                                            ];
+                                        $diffs = [
+                                                // diffs
+                                            'diff_tt_buy'  => $yesterdayrate ? $exchangerate->tt_buy - $yesterdayrate->tt_buy : null,
+                                            'diff_tt_sell' => $yesterdayrate ? $exchangerate->tt_sell - $yesterdayrate->tt_sell : null,
+                                            'diff_cash_buy'  => $yesterdayrate ? $exchangerate->cash_buy - $yesterdayrate->cash_buy : null,
+                                            'diff_cash_sell' => $yesterdayrate ? $exchangerate->cash_sell - $yesterdayrate->cash_sell : null,
+                                            'diff_earn_buy'  => $yesterdayrate ? $exchangerate->earn_buy - $yesterdayrate->earn_buy : null,
+                                            'diff_earn_sell' => $yesterdayrate ? $exchangerate->earn_sell - $yesterdayrate->earn_sell : null,
+                                        ];
+                                        if($changehistories->count() > 0){
+
+                                            $count = $changehistories->count();
+                                            $lastchangehistory_tt = $changehistories->where("type",'tt')->first();
+                                            $lastchangehistory_cash = $changehistories->where("type",'cash')->first();
+                                            $lastchangehistory_earn = $changehistories->where("type",'earn')->first();
+                                            // Log::info($lastchangehistory_tt);
+
+                                            if($lastchangehistory_tt){
+                                                $diffs['diff_tt_buy']  = $lastchangehistory_tt ? $exchangerate->tt_buy - $lastchangehistory_tt->buy : null;
+                                                $diffs['diff_tt_sell'] = $lastchangehistory_tt ? $exchangerate->tt_sell - $lastchangehistory_tt->sell : null;
+                                            }
+
+                                            if($lastchangehistory_cash){
+                                                $diffs['diff_cash_buy']  = $lastchangehistory_cash ? $exchangerate->tt_buy - $lastchangehistory_cash->buy : null;
+                                                $diffs['diff_cash_buy'] = $lastchangehistory_cash ? $exchangerate->tt_sell - $lastchangehistory_cash->sell : null;
+                                            }
+
+                                            if($lastchangehistory_earn){
+                                                $diffs['diff_earn_buy']  = $lastchangehistory_earn ? $exchangerate->tt_buy - $lastchangehistory_earn->buy : null;
+                                                $diffs['diff_earn_buy'] = $lastchangehistory_earn ? $exchangerate->tt_sell - $lastchangehistory_earn->sell : null;
+                                            }
+
                                         }
                                         return [
                                             ...$exchangerate->toArray(),
@@ -169,7 +170,7 @@ class ExchangeRatesController extends Controller
                                                     // $prevchangehistory = $idx > 0 ? $changehistories[$idx - 1] : null; // order by asc
 
                                                     $prevchangehistory = $idx < $changehistories->count() - 1 ? $changehistories[$idx + 1] : null;
-                                                    Log::info($prevchangehistory);
+                                                    // Log::info($prevchangehistory);
 
                                                     if($prevchangehistory){
                                                         return [
